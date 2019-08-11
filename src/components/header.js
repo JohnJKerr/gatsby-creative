d607d6afc7096704ae18c42b782133cb6d58fb73
John Kerr <kerr.john@gmail.com>
2019-08-11 22:26:33 +0100

    Added styling

--- src/components/header.js
+++ src/components/header.js
@@ -3,32 +3,50 @@ import PropTypes from "prop-types"
 import React from "react"
 
 const Header = ({ siteTitle }) => (
-  <header
-    style={{
-      background: `rebeccapurple`,
-      marginBottom: `1.45rem`,
-    }}
-  >
-    <div
-      style={{
-        margin: `0 auto`,
-        maxWidth: 960,
-        padding: `1.45rem 1.0875rem`,
-      }}
-    >
-      <h1 style={{ margin: 0 }}>
-        <Link
-          to="/"
-          style={{
-            color: `white`,
-            textDecoration: `none`,
-          }}
-        >
-          {siteTitle}
-        </Link>
-      </h1>
-    </div>
-  </header>
+  <>
+    <nav className="navbar navbar-expand-lg navbar-light fixed-top py-3" id="mainNav">
+      <div className="container">
+        <a className="navbar-brand js-scroll-trigger" href="#page-top">Start Bootstrap</a>
+        <button className="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse"
+                data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false"
+                aria-label="Toggle navigation">
+          <span className="navbar-toggler-icon"></span>
+        </button>
+        <div className="collapse navbar-collapse" id="navbarResponsive">
+          <ul className="navbar-nav ml-auto my-2 my-lg-0">
+            <li className="nav-item">
+              <a className="nav-link js-scroll-trigger" href="#about">About</a>
+            </li>
+            <li className="nav-item">
+              <a className="nav-link js-scroll-trigger" href="#services">Services</a>
+            </li>
+            <li className="nav-item">
+              <a className="nav-link js-scroll-trigger" href="#portfolio">Portfolio</a>
+            </li>
+            <li className="nav-item">
+              <a className="nav-link js-scroll-trigger" href="#contact">Contact</a>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </nav>
+    <header className="masthead">
+      <div className="container h-100">
+        <div className="row h-100 align-items-center justify-content-center text-center">
+          <div className="col-lg-10 align-self-end">
+            <h1 className="text-uppercase text-white font-weight-bold">Your Favorite Source of Free Bootstrap
+              Themes</h1>
+            <hr className="divider my-4"/>
+          </div>
+          <div className="col-lg-8 align-self-baseline">
+            <p className="text-white-75 font-weight-light mb-5">Start Bootstrap can help you build better websites
+              using the Bootstrap framework! Just download a theme and start customizing, no strings attached!</p>
+            <a className="btn btn-primary btn-xl js-scroll-trigger" href="#about">Find Out More</a>
+          </div>
+        </div>
+      </div>
+    </header>
+  </>
 )
 
 Header.propTypes = {
