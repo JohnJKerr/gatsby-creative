d7ce5533507a061800b30e27c9c8ed7a8f99fbea
John Kerr <kerr.john@gmail.com>
2019-08-29 23:04:34 +0100

    Added prefix paths

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,4 +1,5 @@
 module.exports = {
+  pathPrefix: 'gatsby-creative',
   siteMetadata: {
     title: `Gatsby Default Starter`,
     description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
